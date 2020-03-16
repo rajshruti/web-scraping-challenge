63ea42296cea2877c816266701ddb17fc38f673e
rajshruti <shrutiraj.bhu@gmail.com>
2020-03-15 22:17:57 -0400

    Update Mission_to_Mars

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -3,6 +3,7 @@ from splinter import Browser
 import os
 import pandas as pd
 import time
+from selenium import webdriver
 
 def scrape():
     browser = Browser("chrome", executable_path = "chromedriver.exe", headless = True)
@@ -75,5 +76,76 @@ def scrape():
     
     url_hemispheres = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(url_hemispheres)
+    
+    # Getting the base url
+    hemisphere_base_url = "{0.scheme}://{0.netloc}/".format(urlsplit(url_hemispheres))
+    hemisphere_img_urls = []
+    hemisphere_img_urls
 
-    # Remaining code goes here
+    # Cerberus-Hemisphere-image-url
+
+    hemisphere_img_urls = []
+    results = browser.find_by_xpath( "//*[@id='product-section']/div[2]/div[1]/a/img").click()
+    time.sleep(2)
+    cerberus_open_click = browser.find_by_xpath( "//*[@id='wide-image-toggle']").click()
+    time.sleep(1)
+    cerberus_image = browser.html
+    soup = bs(cerberus_image, "html.parser")
+    cerberus_url = soup.find("img", class_="wide-image")["src"]
+    cerberus_img_url = hemisphere_base_url + cerberus_url
+    cerberus_title = soup.find("h2",class_="title").text
+    back_button = browser.find_by_xpath("//*[@id='splashy']/div[1]/div[1]/div[3]/section/a").click()
+    cerberus = {"image title":cerberus_title, "image url": cerberus_img_url}
+    hemisphere_img_urls.append(cerberus)
+
+
+    # Schiaparelli-Hemisphere-image-url
+
+    results1 = browser.find_by_xpath( "//*[@id='product-section']/div[2]/div[2]/a/img").click()
+    time.sleep(2)
+    schiaparelli_open_click = browser.find_by_xpath( "//*[@id='wide-image-toggle']").click()
+    time.sleep(1)
+    schiaparelli_image = browser.html
+    soup = bs(schiaparelli_image, "html.parser")
+    schiaparelli_url = soup.find("img", class_="wide-image")["src"]
+    schiaparelli_img_url = hemisphere_base_url + schiaparelli_url
+    schiaparelli_title = soup.find("h2",class_="title").text
+    back_button = browser.find_by_xpath("//*[@id='splashy']/div[1]/div[1]/div[3]/section/a").click()
+    schiaparelli = {"image title":schiaparelli_title, "image url": schiaparelli_img_url}
+    hemisphere_img_urls.append(schiaparelli)
+
+
+    # Syrtis Major Hemisphere
+
+    results1 = browser.find_by_xpath( "//*[@id='product-section']/div[2]/div[3]/a/img").click()
+    time.sleep(2)
+    syrtis_major_open_click = browser.find_by_xpath( "//*[@id='wide-image-toggle']").click()
+    time.sleep(1)
+    syrtis_major_image = browser.html
+    soup = bs(syrtis_major_image, "html.parser")
+    syrtis_major_url = soup.find("img", class_="wide-image")["src"]
+    syrtis_major_img_url = hemisphere_base_url + syrtis_major_url
+    syrtis_major_title = soup.find("h2",class_="title").text
+    back_button = browser.find_by_xpath("//*[@id='splashy']/div[1]/div[1]/div[3]/section/a").click()
+    syrtis_major = {"image title":syrtis_major_title, "image url": syrtis_major_img_url}
+    hemisphere_img_urls.append(syrtis_major)
+
+
+    # Valles Marineris Hemisphere
+
+    results1 = browser.find_by_xpath( "//*[@id='product-section']/div[2]/div[4]/a/img").click()
+    time.sleep(2)
+    valles_marineris_open_click = browser.find_by_xpath( "//*[@id='wide-image-toggle']").click()
+    time.sleep(1)
+    valles_marineris_image = browser.html
+    soup = bs(valles_marineris_image, "html.parser")
+    valles_marineris_url = soup.find("img", class_="wide-image")["src"]
+    valles_marineris_img_url = hemisphere_base_url + syrtis_major_url
+    valles_marineris_title = soup.find("h2",class_="title").text
+    back_button = browser.find_by_xpath("//*[@id='splashy']/div[1]/div[1]/div[3]/section/a").click()
+    valles_marineris = {"image title":valles_marineris_title, "image url": valles_marineris_img_url}
+    hemisphere_img_urls.append(valles_marineris)
+
+    mars_facts_data["hemisphere_img_url"] = hemisphere_img_urls
+
+    return mars_facts_data
